3fc4410c0f4b0469eebd94451d7b4b13d79da78e
Felipe Tavares <xfelipetavares@gmail.com>
2023-03-30 16:53:41 -0300

    fix: browserRouter wrapping the App

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import { Provider } from 'react-redux'
 import App from './App'
 import './style/global.scss'
 import store from './redux/store'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
 )
