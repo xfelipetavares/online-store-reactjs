5c8e3d754bddcf68f4dfb4a4350add4fabd3de75
Felipe Tavares <xfelipetavares@gmail.com>
2023-03-30 20:19:51 -0300

    wip: creating product cards

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
-
-// import { Container } from './styles';
+import React, { useEffect, useState } from 'react'
+import { getProductsImage } from '../../helpers/mercado_livre_api'
+import styles from './styles.module.scss'
 
 const ProductCard = ({ product }) => {
-  return <div>{product.title}</div>
+  const [urlImage, setUrlImage] = useState([])
+
+  const getImg = async () => {
+    const teste = await getProductsImage(product.thumbnail_id)
+    setUrlImage(teste)
+  }
+  // const dispatch = useDispatch()
+
+  useEffect(() => {
+    getImg()
+  }, [])
+
+  return (
+    <div className={styles.card}>
+      <p className={styles.title}>{product.title}</p>
+      <div className={styles.boxImg}>
+        {product.shipping.free_shipping && (
+          <p className={styles.free_shipping}>Frete Grátis</p>
+        )}
+        <img src={urlImage} alt={product.title} className={styles.image} />
+      </div>
+      <button className={styles.button} type="button">
+        Adicionar ao carrinho
+      </button>
+    </div>
+  )
 }
 
 export default ProductCard
