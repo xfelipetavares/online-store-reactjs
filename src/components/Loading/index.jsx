5bb47ef12f897ddd06c4349e5712d5e61e8ef804
Felipe Tavares <xfelipetavares@gmail.com>
2023-04-02 14:18:56 -0300

    loading animation created

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import loadingImage from '../../assets/loadingImage.svg'
+import styles from './styles.module.scss'
 
-// import { Container } from './styles';
-
-const Loading = () => <p>Loading...</p>
+const Loading = () => (
+  <div className={styles.loading}>
+    <img src={loadingImage} alt="" />
+    <p>Loading...</p>
+  </div>
+)
 
 export default Loading
