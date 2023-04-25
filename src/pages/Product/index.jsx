fb207e489a0e82aa772b1af5861d72c3dc4ee955
Felipe Tavares <xfelipetavares@gmail.com>
2023-04-25 17:50:21 -0300

    top part from product page is done... I guess

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './styles.module.scss'
 import { useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
@@ -32,6 +32,7 @@ const Product = () => {
     questions,
   } = useSelector((store) => store.api.product)
   const p = useSelector((store) => store.api.product.info)
+  const [img, setImg] = useState('')
 
   useEffect(() => {
     dispatch(getProduct(productId))
@@ -40,29 +41,47 @@ const Product = () => {
     dispatch(getQuestions(id))
   }, [id])
 
+  useEffect(() => {
+    setImg(images)
+  }, [images])
+
   return (
     <div className={styles.container}>
       <div className={styles.topProductBox}>
         <div className={styles.topProdLeft}>
           <div className={styles.imgList}>
             {pictures?.map(({ url, id }) => (
-              <img key={id} width={100} src={url} alt={title} />
+              <img
+                key={id}
+                onClick={() => setImg(url)}
+                width={100}
+                src={url}
+                alt={title}
+              />
             ))}
           </div>
-          <img src={images} className={styles.mainProdImg} alt={title} />
+          <img src={img} className={styles.mainProdImg} alt={title} />
         </div>
         <div className={styles.topProdRight}>
-          <p>Produto {condition === 'new' ? 'novo' : 'usado'}</p>
-          <p>{soldQuantity} vendidos</p>
-          <p>{shipping?.free_shipping === true ? 'frete gratis' : ''}</p>
-          <p>{title}</p>
+          <div className={styles.aboveTitleInfos}>
+            <p className={styles.shipping}>
+              {shipping?.free_shipping === true ? 'frete grátis' : ''}
+            </p>
+            <p className={styles.condition}>
+              Produto {condition === 'new' ? 'novo' : 'usado'}
+            </p>
+            <p className={styles.quantity}>{soldQuantity} vendidos</p>
+          </div>
+          <h2 className={styles.title}>{title}</h2>
           {originalPrice && (
-            <p>
-              De: <s>R${originalPrice?.toFixed(2)}</s>
+            <p className={styles.originalPrice}>
+              De: <s>R$ {originalPrice?.toFixed(2)}</s>
             </p>
           )}
-          <p>R${price?.toFixed(2)}</p>
-          <p>em até 12x R${(price / 12)?.toFixed(2)}</p>
+          <h3 className={styles.price}>R$ {price?.toFixed(2)}</h3>
+          <p className={styles.installments}>
+            em até 12x R${(price / 12)?.toFixed(2)}
+          </p>
           <ButtonAdd2Cart p={p} />
         </div>
       </div>
