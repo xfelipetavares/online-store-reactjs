17ee420fb8feb05a41609d9eb7042ce7a1ac7026
Felipe Tavares <xfelipetavares@gmail.com>
2023-04-25 12:59:40 -0300

    main layout modded

--- src/layouts/MainLayout/index.jsx
+++ src/layouts/MainLayout/index.jsx
@@ -3,17 +3,25 @@ import Header from '../../components/Header'
 import Sidebar from '../../components/Sidebar'
 import SrcRoutes from '../../routes'
 import styles from './styles.module.scss'
+import { useLocation } from 'react-router-dom'
 
-const MainLayout = () => (
-  <div className={styles.layout}>
-    <Header />
-    <div className={styles.sidebar}>
-      <Sidebar />
+const MainLayout = () => {
+  const location = useLocation()
+  return (
+    <div className={styles.layout}>
+      <Header />
       <div className={styles.main}>
-        <SrcRoutes />
+        {!location?.pathname.includes('cart') && (
+          <div className={styles.sidebar}>
+            <Sidebar />
+          </div>
+        )}
+        <div className={styles.routes}>
+          <SrcRoutes />
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default MainLayout
