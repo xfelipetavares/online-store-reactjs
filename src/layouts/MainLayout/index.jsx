9e71d62919db30c398d9195dae472a1fe83cc9b5
Giovanna Sousa <giovannasousa54@gmail.com>
2023-05-17 17:47:41 +0200

    wip: not found page

--- src/layouts/MainLayout/index.jsx
+++ src/layouts/MainLayout/index.jsx
@@ -4,22 +4,32 @@ import Sidebar from '../../components/Sidebar'
 import SrcRoutes from '../../routes'
 import styles from './styles.module.scss'
 import { useSelector } from 'react-redux'
+import { useLocation } from 'react-router-dom'
+import NotFound from '../../pages/NotFound'
 
 const MainLayout = () => {
   const isOpen = useSelector((store) => store.sidebar.isOpen)
+  const { pathname } = useLocation()
+
   return (
     <div className={styles.layout}>
-      <Header />
-      <div className={styles.main}>
-        {isOpen && (
-          <div className={styles.sidebar}>
-            <Sidebar />
+      {pathname === '/notfound' ? (
+        <NotFound />
+      ) : (
+        <>
+          <Header />
+          <div className={styles.main}>
+            {isOpen && (
+              <div className={styles.sidebar}>
+                <Sidebar />
+              </div>
+            )}
+            <div className={styles.routes}>
+              <SrcRoutes />
+            </div>
           </div>
-        )}
-        <div className={styles.routes}>
-          <SrcRoutes />
-        </div>
-      </div>
+        </>
+      )}
     </div>
   )
 }
--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-// import { Container } from './styles';
+import notFound from '../../assets/notFound.svg'
+import happy from '../../assets/happy.svg'
+import styles from './styles.module.scss'
+import { useNavigate } from 'react-router-dom'
 
 function NotFound() {
-  return <div />
+  const navigate = useNavigate()
+  const [mood, setMood] = useState(false)
+  const [count, setCount] = useState(4)
+
+  const counter = setInterval(() => {
+    setCount(count - 1)
+  }, 1000)
+
+  return (
+    <div className={styles.notFoundPage}>
+      {!mood ? <h1>404</h1> : <h1>Ok...</h1>}
+      {!mood ? (
+        <h2>Não conseguimos encontrar esta página!</h2>
+      ) : (
+        <h2>Vou redirecionar você :P !</h2>
+      )}
+      <img src={!mood ? notFound : happy} alt="gatinho" />
+
+      <button
+        onClick={() => {
+          setMood(true)
+          setTimeout(() => {
+            clearInterval()
+            navigate('/')
+          }, 3000)
+          counter()
+        }}
+      >
+        {!mood ? 'Voltar para a página inicial' : `${count}...`}
+      </button>
+    </div>
+  )
 }
 
 export default NotFound
